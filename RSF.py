62a06bfd2c9720f1ccd909b18602d568459ed275
35drake <phys152boi@gmail.com>
2024-01-31 15:20:48 -0500

    Browser function created and both py files combined into one. Only the programs actual execution of surveys using spreadsheet data must be done now (plus, the writing to the spreadsheet of submission updates).

--- RSF.py
+++ RSF.py
@@ -17,15 +17,41 @@
 #	handling single-letter variable names, so during debug don't send a command like 
 #	"a = driver.find_element("id","blah").
 
+import pandas # For manipulating Excel data
+# import numpy ...seems like I don't need this because pandas will have numpy type support builtin
+import datetime # For converting Excel dates into strings and getting today's date
+
+import time # For checking time elapsed with time.time() and a possible a pause with time.sleep()
+import getpass # For a hidden password input through getpass.getpass()
+
+# Import tools that I don't understand:
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+# Basically this is used to tell us when HTML buttons aren't clickable
+from selenium.common.exceptions import WebDriverException
+
+# So I can navigate the webpage with tab and space keys (no longer used though):
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.action_chains import ActionChains
+
+# To ask the user for a hidden password
+import getpass
+
 # This will do the login portion of the program
-def login(driver):
-	if 0:
-		# Log in by taking credentials, then pressing the "LOG IN" button
-		driver.find_element("id","username").send_keys(input("Username: "))
-		driver.find_element("id","password").send_keys(getpass.getpass("Password: "))
-	else:
-		driver.find_element("id","username").send_keys("ds001")
-		driver.find_element("id","password").send_keys("fake")
+def login(driver,username,password):
+#	if 0:
+#		# Log in by taking credentials, then pressing the "LOG IN" button
+#		driver.find_element("id","username").send_keys(input("Username: "))
+#		driver.find_element("id","password").send_keys(getpass.getpass("Password: "))
+#	else:
+#		driver.find_element("id","username").send_keys("ds001")
+#		driver.find_element("id","password").send_keys("fakePassword")
+
+
+	driver.find_element("id","username").send_keys(username)
+	driver.find_element("id","password").send_keys(password)
+
 	login_button = driver.find_element("id","login_btn")
 	login_button.click()  # Select the button by clicking on it
 
@@ -64,23 +90,6 @@ def spam_by_x(driver,xpath):
 		except WebDriverException:
 			break
 
-
-import time # For checking time elapsed with time.time() and a possible a pause with time.sleep()
-import getpass # For a hidden password input through getpass.getpass()
-
-# Import tools that I don't understand:
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-
-from selenium.common.exceptions import WebDriverException
-
-# So I can navigate the webpage with tab and space keys:
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-
-
-
-
 # Open the spreadsheet file and edit it so all new rows that the user recently added now say "Not yet entered"
 #	in column L.
 # Next, put all the data from those new rows into an array of data that must be put into forms. Note: if an employee
@@ -89,20 +98,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 #	MULTIPLE strings of data and multiple forms submitted. So you'll have to process the Excel data instead
 #	of just directly importing the grid.
 
-# Drake, put Excel operations here
+# Drake, put Excel operations here(?)
 
+# This function opens up a Chrome tab and attempts to submit a survey with the parameters as the survey options
+def auto_browser(username, password, mask_or_failure_number, submitting_for_real):
+	my_return_value = "NOT SUBMITTED YET" # This will be returned at the end of the auto_browser() function to tell you if the form submitted or not
+	
+	# Make the browser headless and set the URL of the Redcap survey:
+	options = webdriver.ChromeOptions() ; options.headless = True ; url = "https://redcap.partners.org/redcap/plugins/survey_token/survey_token_login.php?pid=18168&hash=988968e7-e9d0-4581-9c1e-0ddd3f5b8036"
 
-
-
-# Load a headless browser and set the URL of the Redcap survey:
-options = webdriver.ChromeOptions() ; options.headless = True ; url = "https://redcap.partners.org/redcap/plugins/survey_token/survey_token_login.php?pid=18168&hash=988968e7-e9d0-4581-9c1e-0ddd3f5b8036"
-
-
-# I used to use the following two lines, but they only work on some computers (perhaps a problem with Chrome versions?)
-#from webdriver_manager.chrome import ChromeDriverManager
-#with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
-# Now, instead of the above two lines, I use the below two lines (the "if 1:" is to keep indentation valid). See here for what led me to this solution: https://stackoverflow.com/questions/76553813/messageunable-to-obtain-chromedriver-using-selenium-manager
-for count in range(1):
 	driver = webdriver.Chrome()
 	
 	# Load the webpage
@@ -110,15 +114,20 @@ for count in range(1):
 
 
 	# Run the login function (defined in this program)
-	login(driver)
+	login(driver, username, password)
 
 	# Print the homepage title and make sure you've reached the homepage
+	time_of_login_attempt = time.time()
 	while 1:
 		print(driver.title)
 		print("\n")
 		if (driver.title == "REDCap"):
 			break
-		
+		if time.time() - time_of_login_attempt > 7:		
+			print("\nLogin has not succeeded after 7 seconds. Maybe you have the incorrect password?\n\n")
+			driver.close()
+			return(my_return_value)
+
 	# Wait until the text input field loads, then type in the employee's ID number:
 	try:
 		driver.find_element("id","search_field").send_keys("100634497")
@@ -184,74 +193,161 @@ for count in range(1):
 	# Click the correct boxes depending on what fit test result is being input right now:
 
 	# A passed 1860S Fit test:
-	if 0:
+	if mask_or_failure_number == 4:
 		spam_by_x(driver,Small_xpath)
 		spam_by_x(driver,Yes_xpath)
 		spam_by_x(driver,Mask1860S_xpath)
 		spam_by_x(driver,N95_xpath)
 	
 	# A passed 1860R Fit test:
-	if 0:	
+	elif mask_or_failure_number == 5:	
 		spam_by_x(driver,MedReg_xpath)
 		spam_by_x(driver,Yes_xpath)
 		spam_by_x(driver,Mask1860R_xpath)
 		spam_by_x(driver,N95_xpath)
 
 	# A passed 1870+ Fit test:
-	if 1:	
+	elif mask_or_failure_number == 6:	
 		spam_by_x(driver,MedReg_xpath)
 		spam_by_x(driver,Yes_xpath)
 		spam_by_x(driver,Mask1870Plus_xpath)
 		spam_by_x(driver,N95_xpath)
 
 	# A passed Halyard Small Fit test:
-	if 0:	
+	elif mask_or_failure_number == 7:	
 		spam_by_x(driver,Small_xpath)
 		spam_by_x(driver,Yes_xpath)
 		spam_by_x(driver,HalyardSmall_xpath)
 		spam_by_x(driver,N95_xpath)
 
 	# A passed Halyard Regular Fit test:
-	if 0:	
+	elif mask_or_failure_number == 8:	
 		spam_by_x(driver,MedReg_xpath)
 		spam_by_x(driver,Yes_xpath)
 		spam_by_x(driver,HalyardRegular_xpath)
 		spam_by_x(driver,N95_xpath)		
 
 	# A completely failed Fit Test due to facial size or shape ("Failed N95 options")
-	if 0: 
+	elif mask_or_failure_number == 9:
 		spam_by_x(driver,Failed_xpath)
 		spam_by_x(driver,No_xpath)		
 		spam_by_x(driver,FacialSize_xpath)
 		spam_by_x(driver,PAPR_xpath)
 
 	# A completely failed Fit Test due to facial hair
-	if 0: 
+	elif mask_or_failure_number == 10:
 		spam_by_x(driver,Failed_xpath)
 		spam_by_x(driver,No_xpath)		
 		spam_by_x(driver,FacialHair_xpath)
 		spam_by_x(driver,PAPR_xpath)
+	else:
+		print("The function auto_browser() did not get a valid option passed as argument mask_or_failure_number.\n")
+		driver.close()
+		return(my_return_value)
 
+	# Wait a few seconds before submitting, just in case the user wants to quickly see what the program's doing on the survey	
+	time.sleep(5)
 
 	# Submit the completed form (make sure this is OFF if you're bug testing)
-	if 0: # set this to 1 if you're running the program for real
+	if submitting_for_real: # set this to 1 if you're running the program for real
 		spam_by_x(driver,Submit_xpath)
 		# Confirm that the form was submitted by checking that you're back to the homepage now
 		time_of_submission = time.time()
 		while 1:
 			if driver.title == "REDCap":
 				print("Form submittal confirmed (for real).\n")
+				my_return_value = "SUBMISSION WORKED"
 				break
 			# Check if 45 seconds have passed since submittal but the homepage still hasn't loaded
 			if time.time() - time_of_submission > 45:
-				exit("Error: cannot confirm that form was submitted") 
+				my_return_value = "SUBMISSION FAILED"
+				print("Error: cannot confirm that form was submitted") 
 	else:
-		unused_var = input("Since we are in bug testing mode, the form will not really be submitted.\nClose the Selenium browser and press ENTER in this terminal to continue.\n(It may take a moment)\n")
 		print("Form submittal confirmed (pretend).\n")
+		my_return_value = "SUBMISSION WORKED (PRETEND)"
 
 	# End program
-	unused_var = input("Program done. Press ENTER to close.\n")
+	driver.close()
+	# unused_var = input("Program done.\n")
 	print("Closing...\n")
+	return(my_return_value)
+
+
+# Make a list of the settings from the settings.txt file
+settings_file = open("files/settings.txt","r")
+settings = []
+for line in settings_file:
+	if line[-1] == "\n": # the lines (but not necessarily the final line) will end in an unneeded newline
+		line = line[0:-1]
+	settings = settings + [line]
+
+# Take only the data items and not the field names
+settings[0] = settings[0][17:] # "Spreadsheet_path:" is a 17 character-long phrase
+settings[1] = settings[1][9:] # This is for Username
+settings[2] = settings[2][55:] # This is for Password
+
+# Leave out the space at the start of the item if one exists
+for count in range(0,len(settings)):
+	if settings[count] != "": # Check if it's empty first, so you don't accessing the first (number 0) character of an empty string
+		if settings[count][0] == " ":
+			settings[count] = settings[count][1:]
+SS_path = settings[0]
+my_username = settings[1]
+my_password = settings[2]
+#print(my_password)
+#for my_char in my_password:
+#	print(ord(my_char))
+
+# Ask for the password if it's not in the settings (left blank in the settings.txt file)
+if my_password == "":
+	my_password = getpass.getpass("Your password is not stored in your settings. Please type in your password now (it will not be stored):")
+	
+# Move the contents of the spreadsheet into the my_data NumPy array
+try:
+	my_data = pandas.read_excel(SS_path, header = None) # "No header" means that the first row is still used. We don't actually need the first row of the spreadsheet, but counting it makes things less confusing for me
+except OSError:
+	exit("\nFile not found. It may be unsynced with OneDrive right now due to someone editing it. Make sure the file has a green check mark in the File Explorer instead of the two blue arrows.\n")
+my_data = my_data.to_numpy()
+SS_rows = my_data.shape[0]
+SS_cols = my_data.shape[1]
+
+# Convert all the dates into strings
+for count in range(5,SS_rows):
+	date_time_example = datetime.datetime(2012, 2, 23, 0, 0) # This is an example variable, which we know is of the type "datetime". We'll use it to compare with other variables to see if those are also variables of the "datetime" type.
+	if type(my_data[count][0]) == type(date_time_example): # Check if the current item (from col 0) is a date object
+		my_data[count][0] = my_data[count][0].strftime("%m/%d/%Y") # Change it into a string
+
+# Check that the NumPy array is working. NOTE: the array naturally uses zero-based numbering which I haven't adjusted. For example, cell 7B's (or colloquially "B7"'s ) value on the spreadsheet can be found at my_data[6,1].
+#print("\nSpreadsheet has",SS_cols,"cols and",SS_rows,"rows.\n")
+#print(my_data)
+#print(my_data[eval(input("Get the cell at row: "))][eval(input("and column: "))],"\n")
+
+# Print relevant spreadsheet contents (the actual employee data)
+for count in range(5,SS_rows):
+	print(my_data[count])
+
+# COLUMNS GUIDE FOR my_data:
+# Col 0 is Date 			(program will READ this)
+# Col 2 is ID number 			(program will READ this)
+# Col 4-8 are Masks 			(program will READ this)
+# Col 9-10 are Failure 			(program will READ this)
+# Col 11 is RSF Completion Info 	(program will READ AND WRITE this)
+# Col 12 is RSF Last Time Accessed 	(program will WRITE this)
+
+# Drake, now for each row of my_data[5:SS_rows], use the item of my_data[5:SS_rows][11] to determine if it needs operation,
+#	and if so use the items of my_data[5:SS_rows][0:11] to send the survey with auto_browser(); 
+#	then write into items my_data[5:SS_rows][11:13] detailling what happened.
+
+
+mask_or_failure_number = 9 # Can be equal to 4,5,6,7,8. Refers to the 1860S, 1860R, 1870+, Halyard S, Halyard R. Can be also equal to 9 or 10. Refers to Fail-By-Face-Shape and Fail-By-Facial-Hair. 
+submitting_for_real = 0 # Set to 0 if you're just debugging and don't wanna actually submit RedCap forms
+
+# Make sure the numeric arguments are vals and not strings
+my_return_value = auto_browser(my_username, my_password, mask_or_failure_number, submitting_for_real)
+
+print("Return value is: ", my_return_value)
+
+# The function auto_browser has returned one of four strings: "NOT SUBMITTED YET", "SUBMISSION FAILED", "SUBMISSION WORKED", "SUBMISSION WORKED (PRETEND)"
 
 
 
@@ -263,6 +359,4 @@ for count in range(1):
 # If you have multiple masks or multiple employee forms to put in, have the Chrome tab open only once and not close
 #	till it's done. That way, you can carry out all the RSF operations in a separate desktop while you do
 #	other computer work yourself, without having the RSF new Chrome window keep popping up in your face.
-
-
 
